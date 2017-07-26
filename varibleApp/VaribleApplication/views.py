1f71393fdb34166dd21294491099cfe3ee2fd417
Matthew-J-Walsh <matthewwalsh2021@u.northwestern.edu>
2017-07-26 14:49:42 -0500

    Update views.py

--- varibleApp/VaribleApplication/views.py
+++ varibleApp/VaribleApplication/views.py
@@ -13,9 +13,9 @@ def index(request):
     return HttpResponse("This is where the app might spawn?")
 
 def runapp(request, func):
-    function_name = str(func.strip("/"))
+    function_name = str(func)
     function_manifest = "manifest_of_" + function_name + ".py.txt"
-    folder = data_loc + "/" + function_name
+    folder = data_loc + "/" + function_name + "/"
     filename = folder + function_manifest
     text_file = open(filename, 'r').read()#str(os.listdir(data_loc))
     return HttpResponse("Running Application: " + function_name + " Pulling from: " + folder + " Computing file: " + filename + " Manifest contains: " + text_file)
