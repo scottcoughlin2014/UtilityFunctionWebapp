73837c6079b72105cdccb75818dc8cbfc720b42d
Matthew-J-Walsh <matthewwalsh2021@u.northwestern.edu>
2017-07-31 12:38:02 -0500

    Update forms.py

--- varibleApp/VaribleApplication/forms.py
+++ varibleApp/VaribleApplication/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
+#not sure how this works or if i even need this... 
+#need templates to work to test out how this works.
 class Input(forms.Form):
     input = forms.CharField(label="input", max_length=250)
