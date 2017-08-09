35254b31371c39bb8f988701ffdc9d5ac1edda61
Matthew Walsh <matthewwalsh2021@u.northwestern.edu>
2017-08-09 13:38:45 -0500

    quick update for post meeting

---
+++ varibleApp/VaribleApplication/testingPyCode.py
@@ -0,0 +1,23 @@
+import importlib
+import os
+
+print(os.getcwd())
+#try:
+print("wtf")
+mod = importlib.import_module("DataFolder.interface.interface")
+print("is going")
+print mod
+print("on")
+#except:
+#    mod = importlib.import_module("urls")
+#except:
+#    try:
+#        mod = __import__("DataFolder.interface.interface")
+#    except:
+#        print(os.path.isfile(os.getcwd()+"\\Datafolder\\interface\\interface.py"))
+#        print(os.path.isfile(os.getcwd()+"\\Datafolder\\interface\\__init__.py"))
+
+#1: 1.15*(1+.1*1)  = 1.265
+#2: 1.15*(1+.1*2)  = 1.38
+#3: 1.15*(1+.1*2)  = 1.38
+#4: 1.15*(1+.1*4/3)= 1.298888
